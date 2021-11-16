6133b26028cbb10ee79d75a32665a60a8ef94628
kiranes8c <phamdat1207@gmail.com>
2021-11-16 22:17:23 +0700

    TD_model_Users

---
+++ models/user.js
@@ -0,0 +1,10 @@
+// Mongoose db Connection
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+var User = mongoose.model('User', userSchema);
+module.exports = User;
